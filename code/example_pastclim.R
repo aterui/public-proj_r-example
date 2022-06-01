acbd6e4c62e5fd65f03e0366f2c4332007975f9c
aterui <hanabi0111@gmail.com>
2022-06-01 11:54:19 -0400

    Create example_pastclim.R

---
+++ code/example_pastclim.R
@@ -0,0 +1,61 @@
+
+# setup -------------------------------------------------------------------
+## install pastclim package from GitHub
+## install pastclimData for easy data download
+devtools::install_github("EvolEcolGroup/pastclim")
+devtools::install_github("EvolEcolGroup/pastclimData", ref="master")
+
+## load libraries
+## pacman::p_load() allows you to load multiple libraries at once.
+pacman::p_load(pastclim,
+               pastclimData,
+               tidyverse)
+
+## download Beyer2020 data
+## this will take ~ 15 mins to complete
+download_dataset(dataset="Beyer2020",
+                 bio_variables = c("bio01","bio05"))
+
+
+# pastclim example data ---------------------------------------------------
+
+## file path to the example file
+path_to_example_nc <- system.file("/extdata/", package="pastclim")
+
+## get a summary of downloaded data
+get_downloaded_datasets(path_to_nc = path_to_example_nc)
+
+## make a list of lat lon & time_bp
+locations <- data.frame(longitude = c(0, 90, -120, -7),
+                        latitude = c(20, 45, 60, 37),
+                        time_bp = c(0, -10000, -20000, -10000))
+
+## climate data for a specific location & time
+## `x` for lat lon
+## `time_bp` for time
+climate_for_locations(x = locations[,c("longitude", "latitude")],
+                      time_bp = locations$time_bp,
+                      bio_variables = c("bio01","bio12"),
+                      dataset = "Example",
+                      nn_interpol = FALSE,
+                      path_to_nc = path_to_example_nc)
+
+## time-series climate data for a specific location
+locations_ts <- time_series_for_locations(x = locations[1, c("longitude", "latitude")],
+                                          bio_variables=c("bio01","bio12"),
+                                          dataset = "Example",
+                                          path_to_nc = path_to_example_nc)
+
+## get a raster layer for a specific time slice
+climate_20k <- climate_for_time_slice(time_bp = -20000,
+                                      c("bio01","bio10","bio12"),
+                                      dataset = "Beyer2020",
+                                      path_to_nc = path_to_example_nc)
+
+
+# tidyverse example -------------------------------------------------------
+
+iris %>% 
+  as_tibble() %>% 
+  filter(Species == "setosa",
+         Sepal.Length > 5)
